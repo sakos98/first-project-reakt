9220e9e92e84b6e85f847871a4633e37d7e313fd
Michal Sak <michusakowicz@gmail.com>
2023-06-24 12:46:43 +0200

    repair stars

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,20 +2,25 @@ import styles from './Card.module.scss';
 import { useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import { toggleCardFavorite } from '../../redux/store';
+import { useState } from 'react';
 
 const Card = (props) => {
   const dispatch = useDispatch();
   const cardId = props.cardId;
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
   const toggle = (e) => {
     e.preventDefault();
+    setIsButtonClicked(!isButtonClicked);
     dispatch(toggleCardFavorite(cardId));
   };
 
   return (
     <div className={styles.card}>
       {props.title}
-      <button onClick={toggle} className={styles.button}>
+      <button onClick={toggle}
+      className={clsx(styles.button, { [styles.clicked]: isButtonClicked })}
+      >
       <span className={clsx('fa fa-star-o', props.isFavorite)} />
       </button>
     </div>
